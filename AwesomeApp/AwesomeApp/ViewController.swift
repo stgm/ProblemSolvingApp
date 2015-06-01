4eb912f95d15a22a66c2bb40fc09d58056f693ed
Martijn Stegeman <martijn@stgm.nl>
2015-06-01 11:55:29 +0200

    oke, javascript én een nieuwe app dan

---
+++ AwesomeApp/AwesomeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AwesomeApp
+//
+//  Created by Martijn Stegeman on 01-06-15.
+//  Copyright (c) 2015 Minor Programmeren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AwesomeApp/AwesomeAppTests/AwesomeAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  AwesomeAppTests.swift
+//  AwesomeAppTests
+//
+//  Created by Martijn Stegeman on 01-06-15.
+//  Copyright (c) 2015 Minor Programmeren. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AwesomeAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
